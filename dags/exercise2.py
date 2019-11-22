eb2e8a86df0e3dc557157a4088fe333e49190085
Jonathan Alves <jonathan.f.alves@gmail.com>
2019-11-22 07:13:01 -0300

    exercise 2

---
+++ dags/exercise2.py
@@ -0,0 +1,46 @@
+import airflow
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator, BashOperator, DummyOperator
+from datetime import datetime
+
+dag = DAG(
+        dag_id="exercise2",
+        default_args={
+            "owner":"jonathan",
+            "start_date":airflow.utils.dates.days_agor(3),
+            },
+        )
+
+def print_timestamp():
+    print(datetime.now())
+
+print_timestamp = PythonOperator(
+        task_id="print_timestamp",
+        python_callable=print_timestamp(),
+        provide_context=False,
+        dag=dag)
+
+wait_1 = BashOperator(
+        task_id="wait_1",
+        bash_command="sleep 1",
+        dag=dag
+        )
+
+wait_5 = BashOperator(
+        task_id="wait_1",
+        bash_command="sleep 5",
+        dag=dag
+        )
+
+wait_10 = BashOperator(
+        task_id="wait_10",
+        bash_command="sleep 10",
+        dag=dag
+        )
+
+the_end = DummyOperator(
+        task_id="the_end",
+        dag=dag
+        )
+
+print_timestamp >> [wait_1,wait_5,wait_10] >> the_end
