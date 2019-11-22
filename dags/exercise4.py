299aeab6584cd1be4a0922463f8bc76665892d97
Jonathan Alves <jonathan.f.alves@gmail.com>
2019-11-22 11:29:13 -0300

    fix exercise 4

--- dags/exercise4.py
+++ dags/exercise4.py
@@ -12,7 +12,7 @@ dag = DAG(
             schedule_interval="0 0 * * *"
         )
 
-postgres_to_google_cloud = PostgresToGoogleCloudOperator(
+postgres_to_google_cloud = PostgresToGoogleCloudStorageOperator(
         task_id="postgres_to_google_cloud",
         sql="SELECT * FROM land_registry_price_paid_uk WHERE transfer_date = '{{ ds }}'",
         bucket="airflow-training-data-jalves",
