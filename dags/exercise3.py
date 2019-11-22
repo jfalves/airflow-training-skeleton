10f4f50c66da5aabf591bb2f46621e599f8efdf4
Jonathan Alves <jonathan.f.alves@gmail.com>
2019-11-22 11:19:18 -0300

    exercise 4

--- dags/exercise3.py
+++ dags/exercise3.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
+from datetime import date
 
 dag = DAG(
             dag_id="exercise3",
@@ -15,7 +15,22 @@ dag = DAG(
         )
 
 def print_weekday(**context):
-    print("{{ execution_date }}")
+    print(context["execution_date"])
+
+def _context(**context):
+    weekday_person_to_email = {
+            0: "Bob", # Monday
+            1: "Joe", # Tuesday
+            2: "Alice", # Wednesday
+            3: "Joe", # Thursday
+            4: "Alice", # Friday
+            5: "Alice", # Saturday
+            6: "Alice", # Sunday
+        }
+
+    return "email_"+weekday_person_to_email[date.weekday()].lower()
+
+
 
 print_weekday = PythonOperator(
         task_id="print_weekday",
@@ -23,4 +38,10 @@ print_weekday = PythonOperator(
         provide_context=True,
         dag=dag)
 
-print_weekday
+branching = PythonOperator(
+        task_id="branching",
+        python_callable=_branching,
+        provide_context=True,
+        dag=dag)
+
+print_weekday >> branching
---
+++ dags/exercise4.py
@@ -0,0 +1,25 @@
+import airflow
+from airflow import DAG
+from airflow.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+from datetime import datetime
+
+dag = DAG(
+            dag_id="exercise4",
+            default_args={
+                "owner":"jonathan",
+                "start_date":datetime(2019,9,20),
+            },
+            schedule_interval="0 0 * * *"
+        )
+
+postgres_to_google_cloud = Postgresoperator(
+        task_id="postgres_to_google_cloud",
+        sql="SELECT * FROM land_registry_price_paid_uk WHERE 
+        transfer_date = '{{ ds }}'",
+        bucket="airflow-training-data-jalves",
+        filename="{{ ds }}/properties_{}.json",
+        postgres_conn_id="airflow-training",
+        dag=dag
+        )
+
+postgres_to_google_cloud
