4067648ebe98547ec409815f7bba25efc616e7a2
Jonathan Alves <jonathan.f.alves@gmail.com>
2019-11-22 06:40:33 -0300

    Add Dag object into bonus

--- dags/bonus.py
+++ dags/bonus.py
@@ -1,4 +1,14 @@
+import airflow
+from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+
+dag = DAG(
+        dag_id="bonus",
+        default_args={
+            "owner":"jonathan",
+            "start_date":airflow.utils.dates.days_ago(3),
+            },
+        )
 
 def print_something(**context):
     print('hello!')
